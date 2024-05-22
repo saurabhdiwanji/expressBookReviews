74e31a762c54e8ae52729a41c189951aff38fe2e
Saurabh Diwan <saurabh.diwanji@gmail.com>
2024-05-22 17:02:09 -0400

    task 1 from lab

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,14 +6,25 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.query.username; 
+  const password = req.query.password; 
+
+  if (username && password) { 
+    if (!isValid(username)) { 
+        users.push({username, password}); 
+        return res.status(200).json({message: "User successfully registered. Now you can login."})
+    } else {
+        return res.status(404).json({message: "User already exist!"}); 
+    }
+    
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(books); 
 });
 
 // Get book details based on ISBN
